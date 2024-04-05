fc489fa594b51990899eef53f5585cba2fe98cf7
Tim Pepper <tjpepper93@gmail.com>
2024-04-05 00:57:47 -0400

    troubleshoot deployment

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const baseURL = process.env.BASE_URL + ':' + process.env.PORT;
+const isLocal = process.env.BASE_URL === 'http://localhost'
+const baseURL = process.env.BASE_URL + (isLocal ? ':' + process.env.PORT : '');
 const port = process.env.PORT;
 const secret = process.env.SECRET;
 const clientID = process.env.CLIENT_ID;
